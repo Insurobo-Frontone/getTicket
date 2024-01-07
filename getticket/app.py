2905157248fbee06f832dfe62627c4d2fd0d1262
Keunreol Park <ritter4u@gmail.com>
2024-01-08 01:55:13 +0900

    merge functions

--- getticket/app.py
+++ getticket/app.py
@@ -95,6 +95,79 @@ def getBizInfo():
     
     return response.json()
 
+
+@app.route("/getBizInfoOnce", methods=['POST'])
+def getBizInfoOnce():
+    data = {}
+    file_path = "./ticket.json"
+
+    # try:
+    with open(file_path, 'r+') as file:
+        temp_data = json.load(file)
+
+    now = time.time()
+    dt = datetime.fromtimestamp(now)
+    oldtime = temp_data['time']
+
+    newtime = dt.timestamp()
+
+    olddatetimeobj = datetime.fromtimestamp(oldtime) + timedelta(hours=1)
+    newdatetimeobj = datetime.fromtimestamp(newtime)
+
+    # print('Debug : '+olddatetimeobj.timestamp)
+    # print('Debug : '+newdatetimeobj.timestamp)
+
+    if (newdatetimeobj > olddatetimeobj):
+        clientId = os.environ.get('clientId')
+        clientSecret = os.environ.get('clientSecret')
+        url = "https://api.moneypin.biz/bizno/v1/auth/token"
+
+        payload = json.dumps({
+            "grantType": "ClientCredentials",
+            "clientId": clientId,
+            "clientSecret": clientSecret
+        })
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        data = {
+            "tokenset": response.json(),
+            "time": newtime
+        }
+        print('Debug: key to Store')
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file)
+    else:
+        data = temp_data
+        print('Debug: Stored key')
+
+    res = {
+        "token": data['tokenset']['token']
+    }
+
+    token=res['token']
+
+    content_type = request.headers.get('Content-Type')
+    accept = request.headers.get('accept')
+    authorization = "Authorization: Bearer "+token
+
+    url = "https://api.moneypin.biz/bizno/v1/biz/info/base"
+
+    payload = json.dumps(request.json)
+    headers = {
+        'Content-Type': content_type,
+        'Accept': accept,
+        'Authorization': authorization
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return response.json()
+
 @app.after_request
 def apply_caching(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
