2436b4291aa3810b5df7b38d5037f8cf38aa1e24
Keunreol Park <ritter4u@gmail.com>
2024-03-21 10:43:26 +0900

    db exception

--- getticket/models/model.py
+++ getticket/models/model.py
@@ -1,8 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects.mysql import CHAR, DATETIME, INTEGER, TINYINT, VARCHAR
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import BigInteger, CHAR, Column, DECIMAL, Date, DateTime, Double, ForeignKeyConstraint, Index, Integer, LargeBinary, MetaData, String, TIMESTAMP, Table, Text, text
-from sqlalchemy.orm.base import Mapped
+from sqlalchemy import Column, DateTime, MetaData, String, Table, text
 
 metadata = MetaData()
 
